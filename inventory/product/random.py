293add0f2536c962bae007f6a0dd1e22439db5a1
aravindvv0 <77048635+aravindvv0@users.noreply.github.com>
2024-03-05 17:19:30 +0530

    Update random.py

--- inventory/product/random.py
+++ inventory/product/random.py
@@ -2,10 +2,10 @@
 from .models import Product,Category,Brand,Inventory
 from .serializers import ProductSerialaizer,InventorySerializer,InventoryFilter,InventorySerializerWithProduct
 from rest_framework.decorators import api_view,permission_classes
-from rest_framework.response import Response
-from rest_framework import status, viewsets
-from rest_framework.permissions import IsAuthenticated,AllowAny
-from django.contrib.auth.models import User
+# from rest_framework.response import Response
+# from rest_framework import status, viewsets
+# from rest_framework.permissions import IsAuthenticated,AllowAny
+# from django.contrib.auth.models import User
 # from rest_framework.authtoken.models import Token
 # from rest_framework.views import APIView
 # from django.contrib.auth import authenticate
